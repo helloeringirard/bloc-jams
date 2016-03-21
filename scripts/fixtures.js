06f82099bd725ac496b8545abcd0ae06ddaddbf4
Erin Girard <helloeringirard@gmail.com>
2016-03-21 16:19:30 -0700

    checkpoint 32 complete

---
+++ scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumSquirrel = {
+     title: 'This is Nuts',
+     artist: 'Rad Squirrels',
+     label: 'Rodent Rap',
+     year: '2016',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'I like trees.', duration: '4:26' },
+         { title: 'I like nuts.', duration: '3:14' },
+         { title: 'I like other squirrels.', duration: '5:01' },
+         { title: 'I like stealing your snacks.', duration: '3:21'},
+         { title: 'Where did I bury that nut?', duration: '2:15'}
+     ]
+ };
+
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ]
+ };
+
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
